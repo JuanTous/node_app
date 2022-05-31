38d8ef06cf2e0b0cb82bc7108c41f6a8fc0eb8e7
juantous <tousjuan01@gmail.com>
2022-05-30 19:36:40 -0500

    Passport JWT

--- app.js
+++ app.js
@@ -12,11 +12,11 @@ const cookieParser = require('cookie-parser');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 var passport = require('passport');
-var authenticate = require('./authenticate');
+var config = require('./config');
 
 const app = express();
 
-const url = 'mongodb://localhost:27017/conFusion';
+const url = config.mongoUrl;
 const connect = mongoose.connect(url);
 
 connect.then((db) => {
@@ -39,22 +39,8 @@ app.use(passport.session());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-const auth = (req, res, next) => {
-  console.log(req.user);
-
-  if (!req.user) {
-    var err = new Error('You are not authenticated!');
-    err.status = 403;
-    next(err);
-  }
-  else {
-    next();
-  }
-}
-
 app.use(cookieParser('12345-67890-09876-54321'));
 app.use(morgan('dev'));
-app.use(auth);
 app.use(express.static(__dirname + '/public'));
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
