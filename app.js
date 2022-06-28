0ccdcf712b45321dd733e95a985248999cd2578a
Juan Tous <tousjuan01@gmail.com>
2022-06-27 20:15:31 -0500

    Update

--- app.js
+++ app.js
@@ -58,9 +58,6 @@ app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
 app.use('/imageUpload',uploadRouter);
 app.use('/favorites',favoriteRouter);
-app.use('/privacy', (req, res) => {
-  res.sendFile(__dirname + '/views/privacy.html');
-});
 app.use(bodyParser.json());
 
 const server = http.createServer(app);
